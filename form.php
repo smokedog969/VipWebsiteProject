a3cbfcdd24b120fb4cf72e6fad35bdf48c1ed48d
Steffen Schafrik <steffen.schafrik@googlemail.com>
2020-12-14 16:41:00 +0100

    form.php modified

--- form.php
+++ form.php
@@ -4,7 +4,7 @@ var_dump($_POST);
 mail(
     "steffen.schafrik@googlemail.com",
      "Es gibt eine neue Anfrage",
-     "Email: {$_POST['email']}, Name: {$_POST['name']}, Nachricht: {$_POST['message']}" );
+     "Email: {$_POST['email']}, Name: {$_POST['name']}, Message: {$_POST['message']}" );
 
      header("Location: index.php?contacted=1");
 ?>
